d14ed9b171c9d880f508db52ceaeb31310983d32
Samir Hamadi <samir.hamadi@reaktor.com>
2025-04-25 21:54:31 +0300

    Add rectangle to indicate detected face

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,82 @@
 use opencv::{
+    core,
+    highgui,
+    imgproc,
+    objdetect,
     prelude::*,
     videoio,
-    highgui,
     Result,
 };
 
 fn main() -> Result<()> {
-    // Open the default camera (index 0)
-    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?; // 0 is default cam
+    // Open webcam
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
     if !videoio::VideoCapture::is_opened(&cam)? {
         panic!("Unable to open default camera!");
     }
 
-    // Create a window
+    // Load Haar Cascade for face detection
+    let mut face_cascade = objdetect::CascadeClassifier::new(
+        "/opt/homebrew/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"
+    )?;
+
     highgui::named_window("webcam", highgui::WINDOW_AUTOSIZE)?;
 
     loop {
         let mut frame = Mat::default();
         cam.read(&mut frame)?;
-
-        if frame.size()?.width > 0 {
-            highgui::imshow("webcam", &frame)?;
+        if frame.size()?.width == 0 {
+            continue;
         }
 
-        // Break on 'q' key
+        // Convert frame to grayscale
+        let mut gray = Mat::default();
+        imgproc::cvt_color(
+            &frame,
+            &mut gray,
+            imgproc::COLOR_BGR2GRAY,
+            0,
+            core::AlgorithmHint::ALGO_HINT_DEFAULT
+        )?;
+
+        // Detect faces
+        let mut faces = core::Vector::<core::Rect>::new();
+        face_cascade.detect_multi_scale(
+            &gray,
+            &mut faces,
+            1.1,
+            3,
+            0,
+            core::Size::new(30, 30),
+            core::Size::new(0, 0),
+        )?;
+
+        // Draw rectangles around faces
+        for face in faces {
+            imgproc::rectangle(
+                &mut frame,
+                face,
+                core::Scalar::new(0.0, 255.0, 0.0, 0.0), // green
+                2,
+                imgproc::LINE_8,
+                0,
+            )?;
+
+            imgproc::put_text(
+                &mut frame,
+                "Face",
+                core::Point::new(face.x, face.y - 10), // Slightly above the rectangle
+                imgproc::FONT_HERSHEY_SIMPLEX,
+                0.5, // font scale
+                core::Scalar::new(0.0, 255.0, 0.0, 0.0), // same green color
+                1, // thickness
+                imgproc::LINE_AA,
+                false,
+            )?;
+        }
+
+        highgui::imshow("webcam", &frame)?;
+
         let key = highgui::wait_key(10)?;
         if key == 'q' as i32 {
             break;
